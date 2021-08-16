da4340edf20050d54fab279de3f4197bd9d01f8d
arunthomasalex <arunthomasalex@gmail.com>
2021-08-16 14:46:06 +0530

    Added TagContent.java

--- src/main/java/org/example/tutorial/puzzle/TagContent.java
+++ src/main/java/org/example/tutorial/puzzle/TagContent.java
@@ -6,6 +6,23 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/*
+Input
+-----
+4
+<h1>Nayeem loves counseling</h1>
+<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>
+<Amee>safat codes like a ninja</amee>
+<SA premium>Imtiaz has a secret crush</SA premium>
+
+Output
+------
+Nayeem loves counseling
+Sanjay has no watch
+So wait for a while
+None
+Imtiaz has a secret crush
+*/
 public class TagContent {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
