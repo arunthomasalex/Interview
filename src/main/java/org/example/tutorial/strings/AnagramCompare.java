924b2defa9df07fb24f7281baa0969b08bd09c85
Arun T. Alex <arunthomasalex@gmail.com>
2021-09-15 16:24:01 +0530

    Update AnagramCompare.java

--- src/main/java/org/example/tutorial/strings/AnagramCompare.java
+++ src/main/java/org/example/tutorial/strings/AnagramCompare.java
@@ -10,7 +10,6 @@ public class AnagramCompare {
         Arrays.sort(aarray);
         char[] barray = b.toCharArray();
         Arrays.sort(barray);
-        String.valueOf(aarray).equals(String.valueOf(barray));
         System.out.println("Anagram: " + String.valueOf(aarray).equals(String.valueOf(barray)));
     }
 }
