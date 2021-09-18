0b050694ba37ef01d788cb3fca10c84aae9d6e2e
arunthomasalex <arunthomasalex@gmail.com>
2021-09-18 09:21:46 +0530

    Minor change

--- src/main/java/org/example/tutorial/strings/SubSets.java
+++ src/main/java/org/example/tutorial/strings/SubSets.java
@@ -7,12 +7,11 @@ public class SubSets {
     public static void main(String[] args) {
         char[] values = {'a', 'b', 'c'};
         IntStream.range(1, (1 << values.length)).forEach(i -> { //0 to 2 ^ (length of array)
-            System.out.println("{ " +
+            System.out.println(
                     IntStream.range(0, values.length) // 0 to length of array
                             .filter(j -> (i & (1 << j)) > 0) // it is used to check which all bits are enables
-                            .map(j -> values[j])
-                            .mapToObj(v -> String.valueOf((char)v))
-                            .collect(Collectors.joining(", ")) + " }"
+                            .mapToObj(j -> String.valueOf((char)values[j]))
+                            .collect(Collectors.toList()) 
             );
         });
     }
